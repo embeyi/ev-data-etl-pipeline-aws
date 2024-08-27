558d995477d2ee5ab89c68fe94c1fa1f23844a1b
embeyi <133457406+embeyi@users.noreply.github.com>
2024-08-27 12:05:06 +0500

    Create func-extract-source-data.py

---
+++ lambda/func-extract-source-data.py
@@ -0,0 +1,44 @@
+import json
+import os
+import boto3
+import requests
+from datetime import datetime
+from urllib.parse import urlparse
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    # List of API URLs
+    api_urls = [
+        'https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD'
+        # 'https://data.wa.gov/api/views/3d5d-sdqb/rows.csv?accessType=DOWNLOAD'
+        # Add more URLs as needed
+    ]
+    
+    bucket_name = 'aws-s3-dap-landing-dev'
+    
+    for api_url in api_urls:
+        # Fetch the data from the API
+        response = requests.get(api_url)
+        
+        if response.status_code == 200:
+            data = response.content
+            
+            # Parse the URL to create a unique folder name based on the domain and path
+            parsed_url = urlparse(api_url)
+            domain = parsed_url.netloc.replace('.', '_')
+            path = parsed_url.path.replace('/', '_').strip('_')
+            
+            # Define the S3 object key (folder structure) 
+            file_name = f"electric_vehicle_{domain}/{path}"
+            
+            s3.put_object(Bucket=bucket_name, Key=file_name, Body=data)
+            
+            print(f"File uploaded successfully to {bucket_name}/{file_name}")
+        else:
+            print(f"Failed to fetch data from the API: {api_url}, Status Code: {response.status_code}")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps("Files uploaded successfully")
+    }
